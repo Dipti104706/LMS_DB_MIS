e6f05dd1840a8327507e6d19637780626e710b9e
Dipti104706 <diptimayeebehura104@gmail.com>
2021-12-06 22:32:40 +0530

    [Dipti]Added: clustered index create added

---
+++ TableIndexing.sql
@@ -0,0 +1,26 @@
+use CPULog;
+
+select * from dbo.[CpuLogData2019-11-17-new];
+--------------------Create index-------------
+create index in_retrieveCPU   -------it create a non cluster index
+on dbo.[CpuLogData2019-11-17-new](cpu_avg_load_over_5_min Asc); -----sorting in ascending order----
+-----select * , after using index to that column----
+select * from dbo.[CpuLogData2019-11-17-new]
+where cpu_avg_load_over_5_min>1 and cpu_avg_load_over_5_min<1.5;
+
+-----------------Seeing the time slabs , of cpu taken to retrieve data from the table----
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+select * from dbo.[CpuLogData2019-11-17-new]
+where cpu_avg_load_over_5_min>1 and cpu_avg_load_over_5_min<1.5;
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+
+--------------------Clustered index--------
+use LMSdata;
+create clustered index In_cluster
+on Candidates_Education_Det_Check(id Asc,field_name Asc); ----multiple columns-----
+
+select * from Candidates_Education_Det_Check;
+
+---------Non-clustered index----------
